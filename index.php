50c7842a963f88e21a9f8eb509ab6ab96f58ddbc
Adam Burkenpas <aburkenpas@yahoo.com>
2014-04-16 16:17:10 -0400

    created header and footer include files

--- index.php
+++ index.php
@@ -53,46 +53,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 
+include("inc/header.php");
+
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Bottle Opener</title>
-	<link rel="stylesheet" type="text/css" href="stylesheets/style.css" />
 
-	<!-- Google Fonts -->
-
-	<link href='http://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Kite+One' rel='stylesheet' type='text/css'>
-	<link href='http://fonts.googleapis.com/css?family=Nunito' rel='stylesheet' type='text/css'>
-
-	<!-- End Google Fonts-->
-
-
-	<!-- JQuery *Most likely going to remove script, because of multiple pages-->
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script>
-		$(function() {
-		$('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-		var target = $(this.hash);
-		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		if (target.length) {
-		$('html,body').animate({
-		scrollTop: target.offset().top
-		}, 1000);
-		return false;
-			}
-		}
-			});
-		});
-	</script>
-	
-	<!-- End JQuery -->
-
-</head>
 
 <body>
 
@@ -238,23 +202,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 				</div>
 			</section>
 			
-		<!-- footer -->
-
-		<footer>
-		
-				<a href="twitter.com" class="twitter">
-					<img src="img/twitter.png" alt="">
-				</a>
-
-				<a href="facebook.com">
-					<img src="img/facebook.png" alt="">
-				</a>
-		
-			<p>&copy; 2014 Company Name</p>
-		</footer> 
-
-		<!-- End Footer -->
-	</div>
-	
-</body>
-</html>
+<?php include("inc/footer.php"); ?>
